5877225e6c9b77c98ee77fc8e3a4074707b37323
Dagestan05 <steklofan@gmail.com>
2019-08-17 06:24:07 +0200

    initial setup

---
+++ src/pages/Rooms.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Rooms = () => {
+  return (
+    <div>
+      Hello from rooms js
+    </div>
+  );
+};
+
+export default Rooms;
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SingleRoom extends Component {
+  render() {
+    return (
+      <div>
+        hello from Single room page
+      </div>
+    );
+  }
+}
+
+export default SingleRoom;
