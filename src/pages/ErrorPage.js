9204649accb6fe721098765c260e8588aa1c1bae
Dagestan05 <steklofan@gmail.com>
2019-08-17 07:29:11 +0200

    added Banner & hero banner

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
 
 const ErrorPage = () => {
   return (
-    <div>
-      hello from Error page
-    </div>
+    <Hero>
+      <Banner title="404" subtitle="page not found">
+        <Link to="/" className="btn-primary">Retun Home</Link>
+      </Banner>
+    </Hero>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
 
 const Home = () => {
   return (
-    <div>
-      hello from home page
-    </div>
+    <Hero>
+      <Banner title="Luxurious Rooms" subtitle="deluxe rooms starting at $199">
+        <Link to="/rooms" className="btn-primary">Rooms</Link>
+      </Banner>
+    </Hero>
   );
 };
 
