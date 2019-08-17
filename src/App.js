1170b8b88514e8d46955ea177e916f1d1e1b7150
Dagestan05 <steklofan@gmail.com>
2019-08-17 07:02:36 +0200

    added Routes & Navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import {Route, Switch, Link} from 'react-router-dom'
 import './App.css';
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import ErrorPage from './pages/ErrorPage';
-
-
+//Components
+import Navbar from './components/Navbar';
+  
 function App() {
   return (
     <>
-      <Home/>
-      <Rooms/>
-      <SingleRoom/>
-      <ErrorPage/>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/rooms" component={Rooms}/>
+        <Route exact path="/rooms/:slug" component={SingleRoom}/>
+        <Route component={ErrorPage}/>
+      </Switch>
     </>
   );
 }
