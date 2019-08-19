9cf59f3f5c0ee06064777ee252b4f172f65636cd
Dagestan05 <steklofan@gmail.com>
2019-08-19 08:05:00 +0200

    finish gonfiguring context, add Loading compnt, finished featured rooms compnt

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
-import { RoomContext, RoomConsumer } from '../context';
+import { RoomContext } from '../context';
+import Title from './Title';
+import Loading from './Loading';
+import Room from './Room';
 
 class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    const value = this.context;
+    let { loading, featuredRooms: rooms } = this.context;
+    rooms = rooms.map(room=>{
+      return <Room key={room.id} room={room} />
+    })
+    //Return
     return (
-      <div>
-        <p>{value.name}</p>
-      </div>
+      <section className="featured-rooms">
+        <Title title="Featured Rooms"/>
+        <div className="featured-rooms-center">
+          {loading? <Loading/>: rooms}
+        </div>
+        
+      </section>
     );
   }
 }
--- src/context.js
+++ src/context.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+//local data for development
+import items from "./data";
 
 //create context
 const RoomContext = React.createContext();
@@ -9,9 +11,34 @@ const RoomContext = React.createContext();
 //create more advanced Provider to use state
 class RoomProvider extends Component {
   state={
-    name: 'Alex',
-    age: 23
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: true
   }
+  componentDidMount() {
+    let rooms = this.formatData(items);
+    let featuredRooms = rooms.filter(room=> room.featured === true);
+    this.setState({
+      rooms,
+      sortedRooms: rooms,
+      featuredRooms,
+      loading: false
+    })
+  }
+  //format data got from server to readable object structure
+  formatData = (itemsArray) =>{
+    let tempItems = itemsArray.map((item)=>{
+      let id = item.sys.id;
+      let imagesUrls = item.fields.images.map(image=>{
+        return image.fields.file.url
+      })
+      let room = {...item.fields, images: imagesUrls, id: id}
+      return room
+    })
+    return tempItems;
+  }
+  
   render() {
     return <RoomContext.Provider value={{...this.state}}>
       {this.props.children}
